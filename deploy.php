2cfe84ec0662b3e501390f89eb10a1ea87d062b0
Goz3rr <goz3rr@kuubstudios.com>
2014-12-15 17:10:34 +0100

    deploy.php toegevoegd

---
+++ deploy.php
@@ -0,0 +1,36 @@
+<?php
+    $commands = array(
+        'echo $PWD',
+        'whoami',
+        'git pull',
+        'git status',
+        'git submodule sync',
+        'git submodule update',
+        'git submodule status',
+    );
+
+    $output = '';
+    foreach($commands AS $command){
+        $tmp = shell_exec($command);
+        $output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
+        $output .= htmlentities(trim($tmp)) . "\n";
+    }
+?>
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+<meta charset="UTF-8">
+<title>GIT DEPLOYMENT SCRIPT</title>
+</head>
+<body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
+<pre>
+ .  ____  .    ____________________________
+ |/      \|   |                            |
+[| <span style="color: #FF0000;">&hearts;    &hearts;</span> |]  | Git Deployment Script v0.1 |
+ |___==___|  /              &copy; oodavid 2012 |
+              |____________________________|
+ 
+<?= $output; ?>
+</pre>
+</body>
+</html>
